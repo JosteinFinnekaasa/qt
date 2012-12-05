9bd46b18d5d9b2af94fd0296c79de582af577879
Peter Zion <peter.zion@gmail.com>
2012-12-05 16:36:11 -0500

    Darwin build fixes

--- cp-configure-darwin.sh
+++ cp-configure-darwin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./configure -release -opensource -shared -fast -no-qt3support -qtnamespace CPSafeQt -qtlibinfix _CPSafe -no-scripttools -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk -arch x86_64
+./configure -release -opensource -shared -fast -no-qt3support -qtnamespace CPSafeQt -qtlibinfix _CPSafe -no-scripttools -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk -arch x86_64
